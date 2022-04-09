9e4da4a369904b5ae2981fb7596ec0498e1de6d6
KaiqueJuvencio <kaiquejuvencio@hotmail.com>
2022-04-08 22:43:40 -0300

    :dart: Adding TestContainer

--- src/test/java/com/br/youtubeOasys/YoutubeOasysApplicationTests.java
+++ src/test/java/com/br/youtubeOasys/YoutubeOasysApplicationTests.java
@@ -1,13 +1,65 @@
 package com.br.youtubeOasys;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.br.youtubeOasys.domain.model.TaskDTO;
+import com.br.youtubeOasys.domain.repository.TaskRepository;
+
+@Testcontainers
 @SpringBootTest
 class YoutubeOasysApplicationTests {
+	
+	@Autowired
+	private TaskRepository taskRespository;
+	
+	@Container
+	public static PostgreSQLContainer container = new PostgreSQLContainer()
+			.withUsername("postgres")
+			.withPassword("Postgres2019!")
+			.withDatabaseName("postgresql");			
+	
+	@DynamicPropertySource
+	static void properties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", container::getJdbcUrl);
+		registry.add("spring.datasource.password", container::getPassword);
+		registry.add("spring.datasource.username", container::getUsername);
+	}
 
 	@Test
 	void contextLoads() {
+		System.out.println("Context Load");		
 	}
+	
+	@Test
+	void testeDataBase() {
+		TaskDTO task = new TaskDTO();
+		task.setChannelId("TestContainer");
+		task.setId(1L);
+		testDataBaseSave(task);
+		testDataBaseRead();				
+	}
+	
+	public void testDataBaseSave(TaskDTO task) {
+		TaskDTO taskSaved = taskRespository.save(task);
+		assertEquals(taskSaved.getChannelId(), "TestContainer");
+		assertEquals(taskSaved.getId(), 1L);
+	}
+	
+	public void testDataBaseRead() {
+		Optional<TaskDTO> taskRead = taskRespository.findById(1L);
+		assertEquals(taskRead.get().getChannelId(), "TestContainer");
+		assertEquals(taskRead.get().getId(), 1L);	
+	}		
 
 }
