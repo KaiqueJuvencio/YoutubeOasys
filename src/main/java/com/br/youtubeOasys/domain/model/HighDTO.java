115a4b25b964ee59a17cc6c6a496dbae17a4a422
KaiqueJuvencio <kaiquejuvencio@hotmail.com>
2022-04-10 22:05:07 -0300

    :dart: Add lombok on DTOs

--- src/main/java/com/br/youtubeOasys/domain/model/HighDTO.java
+++ src/main/java/com/br/youtubeOasys/domain/model/HighDTO.java
@@ -2,29 +2,12 @@ package com.br.youtubeOasys.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.Data;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class HighDTO {
-	
+@Data
+public class HighDTO {	
 	private String url;
 	private String width;
-	private String height;
-	
-	public String getUrl() {
-		return url;
-	}
-	public String getWidth() {
-		return width;
-	}
-	public String getHeight() {
-		return height;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public void setWidth(String width) {
-		this.width = width;
-	}
-	public void setHeight(String height) {
-		this.height = height;
-	}		
+	private String height;		
 }
--- src/main/java/com/br/youtubeOasys/domain/model/ItemIdDTO.java
+++ src/main/java/com/br/youtubeOasys/domain/model/ItemIdDTO.java
@@ -2,21 +2,11 @@ package com.br.youtubeOasys.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.Data;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class ItemIdDTO {
 	private String kind;
-	private String videoId;
-	
-	public String getKind() {
-		return kind;
-	}
-	public String getVideoId() {
-		return videoId;
-	}
-	public void setKind(String kind) {
-		this.kind = kind;
-	}
-	public void setVideoId(String videoId) {
-		this.videoId = videoId;
-	}    			    
+	private String videoId;   			    
 }
